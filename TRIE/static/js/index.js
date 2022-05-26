e6bb263e6834a4fc5b93170ed62a2e196cc69de6
Aditya Chauhan <66458303+aditya-2703@users.noreply.github.com>
2022-05-26 19:15:30 +0530

    Update index.js

--- TRIE/static/js/index.js
+++ TRIE/static/js/index.js
@@ -1,3 +1,119 @@
+const cuteAlert = ({
+    type,
+    title,
+    message,
+    img,
+    buttonText = 'OK',
+    confirmText = 'OK',
+    vibrate = [],
+    playSound = null,
+    cancelText = 'Cancel',
+    closeStyle,
+  }) => {
+    return new Promise(resolve => {
+      const existingAlert = document.querySelector('.alert-wrapper');
+  
+      if (existingAlert) {
+        existingAlert.remove();
+      }
+  
+      const body = document.querySelector('body');
+  
+      const scripts = document.getElementsByTagName('script');
+  
+      let src = '';
+  
+      for (let script of scripts) {
+        if (script.src.includes('cute-alert.js')) {
+          src = script.src.substring(0, script.src.lastIndexOf('/'));
+        }
+      }
+  
+      let btnTemplate = `
+      <button class="alert-button ${type}-bg ${type}-btn">${buttonText}</button>
+      `;
+  
+      if (type === 'question') {
+        btnTemplate = `
+        <div class="question-buttons">
+          <button class="confirm-button ${type}-bg ${type}-btn">${confirmText}</button>
+          <button class="cancel-button error-bg error-btn">${cancelText}</button>
+        </div>
+        `;
+      }
+  
+      if (vibrate.length > 0) {
+        navigator.vibrate(vibrate);
+      }
+  
+      if (playSound !== null) {
+        let sound = new Audio(playSound);
+        sound.play();
+      }
+  
+      const template = `
+      <div class="alert-wrapper">
+        <div class="alert-frame">
+          ${img !== '' ? '<div class="alert-header ' + type + '-bg">' : '<div>'}
+            <span class="alert-close ${
+              closeStyle === 'circle'
+                ? 'alert-close-circle'
+                : 'alert-close-default'
+            }">X</span>
+            ${img !== '' ? '<img class="alert-img" src="/static/image/success.svg" />' : ''}
+          </div>
+          <div class="alert-body">
+            <span class="alert-title">${title}</span>
+            <span class="alert-message">${message}</span>
+            ${btnTemplate}
+          </div>
+        </div>
+      </div>
+      `;
+  
+      body.insertAdjacentHTML('afterend', template);
+  
+      const alertWrapper = document.querySelector('.alert-wrapper');
+      const alertFrame = document.querySelector('.alert-frame');
+      const alertClose = document.querySelector('.alert-close');
+  
+      if (type === 'question') {
+        const confirmButton = document.querySelector('.confirm-button');
+        const cancelButton = document.querySelector('.cancel-button');
+  
+        confirmButton.addEventListener('click', () => {
+          alertWrapper.remove();
+          resolve('confirm');
+        });
+  
+        cancelButton.addEventListener('click', () => {
+          alertWrapper.remove();
+          resolve();
+        });
+      } else {
+        const alertButton = document.querySelector('.alert-button');
+  
+        alertButton.addEventListener('click', () => {
+          alertWrapper.remove();
+          resolve('ok');
+        });
+      }
+  
+      alertClose.addEventListener('click', () => {
+        alertWrapper.remove();
+        resolve('close');
+      });
+  
+  /*     alertWrapper.addEventListener('click', () => {
+        alertWrapper.remove();
+        resolve();
+      }); */
+  
+      alertFrame.addEventListener('click', e => {
+        e.stopPropagation();
+      });
+    });
+  };
 var data=[]
 
 class Trie_node{
@@ -187,14 +303,22 @@ function add_data(){
     let phone_element = document.getElementById("phone@")
     let name = name_elemenet.value
     let phone = phone_element.value
-    if(name==""){
-        name="unknown"
+    if(name=="" && phone==""){
+      alert("Please Enter Name and Phone Number")
     }
-    let curr_data = {name: name, phone: phone}
-    data.push(curr_data)
-    name_elemenet.value = ""
-    phone_element.value = ""
-    show_data(data)
+    else{
+      let curr_data = {name: name, phone: phone}
+      data.push(curr_data)
+      name_elemenet.value = ""
+      phone_element.value = ""
+      show_data(data)
+      cuteAlert({
+          type:"success",
+          title:"congratulations",
+          message: "contact added successfully",
+          buttonText: "ok",
+      })
+    }
 }
 
 document.querySelector(".search_bar").addEventListener("input",search_data)
